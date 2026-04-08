263239eded038d9f5b772b1d3af1e0d0f7e2dac2
hawon6691 <hawonoj002@gmail.com>
2026-04-08 17:54:30 +0900

    feat: (#38) 인증 세션 저장소 기본 구조 도입

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,13 @@
 import { RouterProvider } from 'react-router';
 
+import AuthSessionBootstrap from './authSessionBootstrap';
 import QueryProvider from './queryProvider';
 import router from './router';
 
 function App() {
   return (
     <QueryProvider>
+      <AuthSessionBootstrap />
       <RouterProvider router={router} />
     </QueryProvider>
   );
