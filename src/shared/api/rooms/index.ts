a9516d16ea1f82e919250f7bbef381921a08dae5
hawon6691 <hawonoj@outlook.kr>
2026-04-10 13:11:27 +0900

    feat: (#73) 방 멤버 강퇴 연결

--- src/shared/api/rooms/index.ts
+++ src/shared/api/rooms/index.ts
@@ -2,10 +2,19 @@ export type {
   CreateRoomRequest,
   GetRoomMembersResponse,
   GetRoomsResponse,
+  KickRoomMemberRequest,
   RoomDetailResponse,
   RoomJoinResponse,
   RoomListItemResponse,
   RoomMemberResponse,
 } from './rooms';
-export { createRoom, getRoomDetail, getRoomMembers, getRooms, joinRoom, leaveRoom } from './rooms';
+export {
+  createRoom,
+  getRoomDetail,
+  getRoomMembers,
+  getRooms,
+  joinRoom,
+  kickRoomMember,
+  leaveRoom,
+} from './rooms';
 export { roomQueries } from './roomsQueries';
