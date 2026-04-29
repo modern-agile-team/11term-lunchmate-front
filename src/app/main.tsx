2f0affe3ab5cdf4cf7a4c32382d3748b5f88175c
hawon6691 <hawonoj002@gmail.com>
2026-04-29 19:26:21 +0900

    Revert "Merge pull request #129 from modern-agile-team/fix/mock-api-network-error/128"
    This reverts commit 7ac872011d53a90b86646e9c72cb5b780e56cf85, reversing
    changes made to dbad15abc7738eb79755bf0726666fea5e2b11df.

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,22 +3,8 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
-async function enableMocking() {
-  if (!import.meta.env.DEV || import.meta.env.VITE_ENABLE_MOCKS === 'false') {
-    return;
-  }
-
-  const { worker } = await import('@/mocks/browser');
-
-  return worker.start({
-    onUnhandledRequest: 'bypass',
-  });
-}
-
-void enableMocking().then(() => {
-  createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  );
-});
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
